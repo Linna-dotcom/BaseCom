0645ce3b463fee31b10f2177b7be15f3cc79ef88
Linna <linnalissy11@gmail.com>
2025-10-24 14:01:10 +0200

    ButtonMitAlert hinzufügen

---
+++ ButtonMitAlert/button-alert.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const hi = document.getElementById('hi');
+
+
+    hi.addEventListener('click', function() {
+       
+        alert('Vielen Dank fürs Ausprobieren!');
+    });
+});
